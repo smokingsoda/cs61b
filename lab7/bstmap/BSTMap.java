fc706ae6a5ca6137a6e5310dcbe38b87935fe4c6
Junru Wang <peterwangharuko@gmail.com>
2024-01-30 21:05:46 +0800

    new fix

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -26,10 +26,11 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
     private BSTMap(K key, V value) {
         root = new Node(key, value);
         left = new BSTMap<K, V>();
-        right = new BSTMap<K,V>();
-        branches = new BSTMap[] {left, right};
+        right = new BSTMap<K, V>();
+        branches = new BSTMap[]{left, right};
         size = 1;
     }
+
     @Override
     public void clear() {
         root = null;
@@ -39,43 +40,59 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
         size = 0;
     }
 
-    private BSTMap search(K key) {
+    private BSTMap[] search(BSTMap Parent, K key) {
+        BSTMap[] ChildAndParent = new BSTMap[]{null, null};
         if (root == null) {
-            return this;
+            ChildAndParent[0] = this;
+            ChildAndParent[1] = null;
         } else {
             int cmp = root.key.compareTo(key);
             if (cmp > 0) {
-                return left;
+                ChildAndParent[0] = left;
+                ChildAndParent[1] = this;
             } else if (cmp < 0) {
-                return right;
+                ChildAndParent[0] = left;
+                ChildAndParent[1] = this;
             } else {
-                return this;
+                ChildAndParent[0] = this;
+                ChildAndParent[1] = Parent;
             }
         }
+        return ChildAndParent;
     }
+
     private void setMap(BSTMap targetMap, K key, V value) {
         targetMap.root = new Node(key, value);
         targetMap.left = new BSTMap();
         targetMap.right = new BSTMap();
-        targetMap.branches = new BSTMap[]{left, right};
+        targetMap.branches = new BSTMap[]{targetMap.left, targetMap.right};
         targetMap.size += 1;
+    }
+
+    private void setMap(BSTMap targetMap, K key, V value, BSTMap left, BSTMap right) {
+        targetMap.root = new Node(key, value);
+        targetMap.left = left;
+        targetMap.right = right;
+        targetMap.branches = new BSTMap[]{targetMap.left, targetMap.right};
+        targetMap.size = 1;
     }
 
     @Override
     public void put(K key, V value) {
-        BSTMap targetMap = search(key);
-        if (targetMap.root == null) {
-            setMap(targetMap, key, value);
-        } else if (targetMap.root.key.equals(key)) {
-            targetMap.root.value = value;
+        BSTMap targetMap[] = search(this, key);
+        if (targetMap[0].root == null) {
+            setMap(targetMap[0], key, value);
+        } else if (targetMap[0].root.key.equals(key)) {
+            targetMap[0].root.value = value;
         } else {
-            targetMap.put(key, value);
+            targetMap[0].put(key, value);
         }
         sizeCal();
     }
 
     private void sizeCal() {
         if (root == null) {
+            size = 0;
             return;
         } else {
             size = 1 + left.size() + right.size();
@@ -84,25 +101,25 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
 
     @Override
     public boolean containsKey(K key) {
-        BSTMap targetMap = search(key);
-        if (targetMap.root == null) {
+        BSTMap targetMap[] = search(this, key);
+        if (targetMap[0].root == null) {
             return false;
-        } else if (targetMap.root.key.equals(key)) {
+        } else if (targetMap[0].root.key.equals(key)) {
             return true;
         } else {
-            return targetMap.containsKey(key);
+            return targetMap[0].containsKey(key);
         }
     }
 
     @Override
     public V get(K key) {
-        BSTMap targetMap = search(key);
-        if (targetMap.root == null) {
+        BSTMap targetMap[] = search(this, key);
+        if (targetMap[0].root == null) {
             return null;
-        } else if (targetMap.root.key.equals(key)) {
-            return (V) targetMap.root.value;
+        } else if (targetMap[0].root.key.equals(key)) {
+            return (V) targetMap[0].root.value;
         } else {
-            return (V) targetMap.get(key);
+            return (V) targetMap[0].get(key);
         }
     }
 
@@ -110,14 +127,137 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
         return size;
     }
 
+    private int validBranches() {
+        int count = -1;
+        if (root == null) {
+            return count;
+        } else {
+            count += 1;
+        }
+        if (left.root != null) {
+            count += 1;
+        }
+        if (right.root != null) {
+            count += 1;
+        }
+        return count;
+    }
+
+    private boolean isLeaf() {
+        return validBranches() == 0;
+    }
+
+    private boolean isValid() {
+        return root != null;
+    }
+
+    private boolean hasLeftLeaf() {
+        if (isValid() && left.root != null) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean hasRightLeaf() {
+        if (isValid() && right.root != null) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public Set<K> keySet() {
         throw new UnsupportedOperationException("Unsupport");
     }
 
+    private boolean isLeft(BSTMap Child) {
+        return left.equals(Child);
+    }
+
+    private boolean isRight(BSTMap Child) {
+        return right.equals(Child);
+    }
+
+    private BSTMap getSuccessor() {
+        if (!right.left.isValid()) {
+            return right;
+        }
+        else return right.goDeepLeft();
+    }
+
+    private BSTMap goDeepLeft() {
+        if (!left.isValid()) {
+            return this;
+        } else {
+            return left.goDeepLeft();
+        }
+    }
+
+    private void exchangeNode(BSTMap otherMap) {
+        K midKey = (K) otherMap.root.key;
+        V midValue = (V) otherMap.root.value;
+        otherMap.root.key = root.key;
+        otherMap.root.value = root.value;
+        root.key = midKey;
+        root.value = midValue;
+    }
+
+    private void resize() {
+        if (!isValid()) {
+            size = 0;
+        } else {
+            left.resize();
+            right.resize();
+            size = 1 + left.size() + right.size();
+        }
+    }
+
+    private void removeHelper(BSTMap parent, BSTMap child, K key) {
+        if (!hasLeftLeaf() && !hasRightLeaf()) {
+            child.clear();
+        } else if (!hasLeftLeaf() && hasRightLeaf()) {
+            if (parent == null) {
+                setMap(child, (K)child.right.root.key, (V)child.right.root.value, child.right.left, child.right.right);
+            }
+            else if (parent.isLeft(child)) {
+                parent.left = child.right;
+            } else {
+                parent.right = child.right;
+            }
+        } else if (hasLeftLeaf() && !hasRightLeaf()) {
+            if (parent == null) {
+                setMap(child, (K)child.left.root.key, (V)child.left.root.value, child.left.left, child.left.right);
+                child.size -= 1;
+            }
+            else if (parent.isLeft(child)) {
+                parent.left = child.left;
+            } else {
+                parent.right = child.left;
+            }
+        } else {
+            BSTMap successor = child.getSuccessor();
+            BSTMap successorParent = child.search(child, key)[1];
+            child.exchangeNode(successor);
+            successor.removeHelper(successorParent, successor, key);
+        }
+    }
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException("Unsupport");
+        BSTMap[] targetMap = search(null, key);
+        if (targetMap[0].root == null) {
+            return null;
+        }
+        else if (targetMap[0].root.key.equals(key)) {
+            V returnValue = (V) targetMap[0].root.value;
+            removeHelper(targetMap[1], targetMap[0], key);
+            sizeCal();
+            return returnValue;
+        }
+        else {
+            V returnValue = (V) targetMap[0].remove(key);
+            sizeCal();
+            return returnValue;
+        }
     }
 
     @Override
--- lab7/bstmap/IteratorTest.java
+++ lab7/bstmap/IteratorTest.java
@@ -1,28 +1,137 @@
 package bstmap;
 
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.*;
 
 public class IteratorTest {
-    @Test
+    //@Test
     public void Test1() {
-        BSTMap actualMap = new BSTMap<Character, Integer>();
-        HashMap expectMap = new HashMap<Character, Integer>();
-        int N = 4; char C = 'a';
+        BSTMap actualMap = new BSTMap<String, Integer>();
+        HashMap expectMap = new HashMap<String, Integer>();
+        ArrayList<String> a = new ArrayList<>();
+        int N = 100000;
         for (int i = 0; i < N; i++) {
-            int randomKey = StdRandom.uniform(1, 10);
-            actualMap.put(C + randomKey, randomKey);
-            expectMap.put(C + randomKey, randomKey);
+            int randomOperation = StdRandom.uniform(0, 100);
+            if (randomOperation < 30) {
+                String C = "hello";
+                int randomKey = StdRandom.uniform(1, 100);
+                actualMap.put(C + randomKey, randomKey);
+                expectMap.put(C + randomKey, randomKey);
+                a.add(C + randomKey);
+            } else if (randomOperation >= 40) {
+                assertEquals(expectMap.size(), actualMap.size());
+            } else if (randomOperation < 90 && a.size() > 0) {
+                int randomIndex = StdRandom.uniform(0, a.size());
+                String randomString = a.remove(randomIndex);
+                assertEquals(expectMap.remove(randomString), actualMap.remove(randomString));
+            } else if (randomOperation >= 95 && a.size() > 0) {
+                int randomIndex = StdRandom.uniform(0, a.size());
+                String randomString = a.get(randomIndex);
+                assertEquals(expectMap.containsKey(randomString), actualMap.containsKey(randomString));
+            }
         }
-        Iterator actual = actualMap.iterator();
-        Iterator expect = expectMap.keySet().iterator();
-        for (int i = 0; i < N; i++) {
-            assertEquals(expect.next(), actual.next());
-        }
-
     }
+    //@Test
+    public void Test2() {
+        BSTMap actualMap = new BSTMap<String, Integer>();
+        TreeMap expectMap = new TreeMap<String, Integer>();
+        ArrayList<String> a = new ArrayList<>();
+        int N = 1000000;
+        for (int i = 0; i < N; i++) {
+            int randomOperation = StdRandom.uniform(0, 100);
+            if (i < N / 2) {
+                String C = "hello";
+                int randomKey = StdRandom.uniform(1, 10000);
+                actualMap.put(C + randomKey, randomKey);
+                expectMap.put(C + randomKey, randomKey);
+                a.add(C + randomKey);
+            } else if (i >= N / 2 && a.size() > 0) {
+                int randomIndex = StdRandom.uniform(0, a.size());
+                String randomString = a.remove(randomIndex);
+                assertEquals(expectMap.containsKey(randomString), actualMap.containsKey(randomString));
+                assertEquals(expectMap.remove(randomString), actualMap.remove(randomString));
+                assertEquals(expectMap.size(), actualMap.size());
+                assertEquals(expectMap.get(randomString), actualMap.get(randomString));
+                assertEquals(expectMap.containsKey(randomString), actualMap.containsKey(randomString));
+            }
+        }
+    }
+    @Test
+    public void Test3() {
+        BSTMap actualMap = new BSTMap<String, Integer>();
+        int N = 10;
+        Stopwatch sw1 = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            String C = "hello";
+            int randomKey = StdRandom.uniform(1, 10000);
+            actualMap.put(C + randomKey, randomKey);
+        }
+        double timeInSecond1 = sw1.elapsedTime();
+        System.out.println("N = " + N + ", Time = " + timeInSecond1);
+        actualMap = new BSTMap<String, Integer>();
+        N = 100;
+        Stopwatch sw2 = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            String C = "hello";
+            int randomKey = StdRandom.uniform(1, 10000);
+            actualMap.put(C + randomKey, randomKey);
+        }
+        double timeInSecond2 = sw1.elapsedTime();
+        System.out.println("N = " + N + ", Time = " + timeInSecond2);
+        N = 500;
+        actualMap = new BSTMap<String, Integer>();
+        Stopwatch sw3 = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            String C = "hello";
+            int randomKey = StdRandom.uniform(1, 10000);
+            actualMap.put(C + randomKey, randomKey);
+        }
+        double timeInSecond3 = sw1.elapsedTime();
+        System.out.println("N = " + N + ", Time = " + timeInSecond3);
+        N = 1000;
+        actualMap = new BSTMap<String, Integer>();
+        Stopwatch sw4 = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            String C = "hello";
+            int randomKey = StdRandom.uniform(1, 10000);
+            actualMap.put(C + randomKey, randomKey);
+        }
+        double timeInSecond4 = sw1.elapsedTime();
+        System.out.println("N = " + N + ", Time = " + timeInSecond4);
+        N = 5000;
+        actualMap = new BSTMap<String, Integer>();
+        Stopwatch sw5 = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            String C = "hello";
+            int randomKey = StdRandom.uniform(1, 10000);
+            actualMap.put(C + randomKey, randomKey);
+        }
+        double timeInSecond5 = sw1.elapsedTime();
+        System.out.println("N = " + N + ", Time = " + timeInSecond5);
+        N = 10000;
+        actualMap = new BSTMap<String, Integer>();
+        Stopwatch sw6 = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            String C = "hello";
+            int randomKey = StdRandom.uniform(1, 10000);
+            actualMap.put(C + randomKey, randomKey);
+        }
+        double timeInSecond6 = sw1.elapsedTime();
+        System.out.println("N = " + N + ", Time = " + timeInSecond6);
+        N = 50000;
+        actualMap = new BSTMap<String, Integer>();
+        Stopwatch sw7 = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            String C = "hello";
+            int randomKey = StdRandom.uniform(1, 10000);
+            actualMap.put(C + randomKey, randomKey);
+        }
+        double timeInSecond7 = sw1.elapsedTime();
+        System.out.println("N = " + N + ", Time = " + timeInSecond7);
+    }
+
 }
