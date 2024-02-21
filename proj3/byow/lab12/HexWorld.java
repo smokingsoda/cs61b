12d70ab13c54081582a6993561a2bcd3a9fac531
Junru Wang <peterwangharuko@gmail.com>
2024-02-21 16:47:02 +0800

    Started with lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,58 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    public static void addHexagon(int length) {
+        if (length <= 0) {
+            System.out.println("Wrong length");
+            System.exit(0);
+        }
+        String returnString = HexWorld.upperHex(length);
+        returnString = returnString + HexWorld.lowerHex(length);
+        System.out.println(returnString);
+
+    }
+    public static String appendSpaces(int number, String inputString) {
+        for (int i = 0; i < number; i++) {
+            inputString += " ";
+        }
+        return inputString;
+    }
+
+    public static String appendTiles(int number, String inputString) {
+        for (int i = 0; i < number; i++) {
+            inputString += "a";
+        }
+        return inputString;
+    }
+
+    public static String upperHex(int length) {
+        int maxLength = length + 2 * (length - 1);
+        String returnString = "";
+        for (int i = length; i <= maxLength; i = i + 2) {
+            returnString = HexWorld.appendSpaces((maxLength - i)/2, returnString);
+            returnString = HexWorld.appendTiles(i, returnString) + "\n";
+        }
+        return returnString;
+    }
+
+    public static String lowerHex(int length) {
+        int maxLength = length + 2 * (length - 1);
+        String returnString = "";
+        for (int i = maxLength; i >= length; i = i - 2) {
+            returnString = HexWorld.appendSpaces((maxLength - i)/2, returnString);
+            if (i == length) {
+                returnString = HexWorld.appendTiles(i, returnString);
+                break;
+            }
+            returnString = HexWorld.appendTiles(i, returnString) + "\n";
+        }
+        return returnString;
+    }
+    @Test
+    public void test() {
+        addHexagon(2);
+        addHexagon(3);
+        addHexagon(4);
+        addHexagon(5);
+    }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -6,20 +6,22 @@ How did your implementation differ from the given one? What lessons can be learn
 
 Answer:
 
------
+ha?
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
 Answer:
 
------
+ok.
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
 Answer:
 
------
+yes
 **What distinguishes a hallway from a room? How are they similar?**
 
 Answer:
+
+no.
